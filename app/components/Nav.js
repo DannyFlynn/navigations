befa6c6fa534ab94aaedc6db75a69094fe679f73
Danny Flynn <flynny386@gmail.com>
2024-06-09 19:03:46 +0100

    main branch  default nav need to update fontsize

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,28 +1,53 @@
-
 "use client"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 const Nav = () => {
 
     const [showNav, setShowNav] = useState(false)
+
+    useEffect(() => {
+
+      if(showNav){
+
+        document.body.style.overflowY = "hidden";
+
+      } else {
+
+        document.body.style.overflowY = "auto";
+
+
+      }
+
+    }, [showNav])
+
   return (
     <nav className="flex justify-between bg-black text-white h-24">
-       <div className="w-1/2 lg:w-3/5 p-4">Nav</div>
-       <button className="absolute top-3 right-3 z-40" onClick={() => setShowNav(!showNav)}>
+       <div className="w-1/2 lg:w-3/5 p-4">
+       <h2>NAV</h2>
+       </div>
+      <ul className="hidden lg:flex justify-evenly w-2/5 p-4">
+            <li><a href="#">link1</a></li>
+            <li><a href="#">link2</a></li>
+            <li><a href="#">link3</a></li>
+            <li><a href="#">link4</a></li>
+      </ul>
+      <button className="flex lg:hidden absolute top-3 right-3 z-40" onClick={() => setShowNav(!showNav)}>
         <span className={`block transition-transform ease-in-out duration-700 ${showNav ? "rotate-180" : "-rotate-180"}`}>
             {showNav ? 'X' : 'M'}
         </span>
       </button>
-     <ul
-        className={`w-full h-screen flex flex-col lg:hidden justify-evenly bg-black absolute z-30 transition-transform ease-in-out duration-700 ${
+      <div className={`w-full h-screen flex lg:hidden  absolute z-30 transition-transform ease-in-out duration-700 ${
           showNav ? "translate-x-0" : "translate-x-full"
-        }`}
+        }`}>
+     <div className="w-1/2 bg-black bg-opacity-50" onClick={() => setShowNav(!showNav)}></div>
+     <ul
+        className="w-1/2 bg-black"
       >
             <li><a href="#">link1</a></li>
             <li><a href="#">link2</a></li>
             <li><a href="#">link3</a></li>
             <li><a href="#">link4</a></li>
         </ul> 
-
+        </div>
     </nav>
   )
 }
