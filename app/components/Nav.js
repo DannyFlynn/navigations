898cc467f640b14bf256ebd09784871f1e790d22
Danny Flynn <flynny386@gmail.com>
2024-06-10 09:48:52 +0100

    nav_leftside_eccom_style_template

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -22,7 +22,7 @@ const Nav = () => {
   return (
     <nav className="flex justify-between bg-black text-white h-24 text-2xl">
        <div className="w-1/2 lg:w-3/5 p-4">
-       <h2>NAV</h2>
+       <h2 className="ml-12">NAV</h2>
        </div>
       <ul className="hidden lg:flex justify-evenly w-2/5 p-4">
             <li><a href="#">link1</a></li>
@@ -30,23 +30,28 @@ const Nav = () => {
             <li><a href="#">link3</a></li>
             <li><a href="#">link4</a></li>
       </ul>
-      <button className="flex lg:hidden absolute top-3 right-3 z-40" onClick={() => setShowNav(!showNav)}>
+      <button className="flex lg:hidden absolute top-3 left-3 z-40" onClick={() => setShowNav(!showNav)}>
         <span className={`block  transition-transform ease-in-out duration-700 ${showNav ? "rotate-180" : "-rotate-180"}`}>
             {showNav ? 'X' : 'M'}
         </span>
       </button>
+      <button className="flex lg:hidden absolute top-3 right-3">
+        <span>
+            Basket
+        </span>
+      </button>
       <div className={`w-full h-screen flex lg:hidden  absolute z-30 transition-transform ease-in-out duration-700 ${
-          showNav ? "translate-x-0" : "translate-x-full"
+          showNav ? "translate-x-0" : "-translate-x-full"
         }`}>
-     <div className="w-1/2 bg-black bg-opacity-50" onClick={() => setShowNav(!showNav)}></div>
      <ul
-        className="w-1/2 bg-black flex flex-col items-center justify-evenly" 
+        className="w-2/3 bg-black flex flex-col items-center justify-evenly" 
       >
             <li><a href="#" >link1</a></li>
             <li><a href="#" >link2</a></li>
             <li><a href="#" >link3</a></li>
             <li><a href="#" >link4</a></li>
         </ul> 
+        <div className="w-1/3 bg-black bg-opacity-50" onClick={() => setShowNav(!showNav)}></div>
         </div>
     </nav>
   )
